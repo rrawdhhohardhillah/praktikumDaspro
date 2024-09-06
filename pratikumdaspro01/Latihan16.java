eef84a13cccd3c41369d2e0d0ae147ede93e0beb
unknown <rrawdhohardhillah@gmail.com>
2024-09-06 09:54:07 +0700

    praktikumdaspro01

---
+++ pratikumdaspro01/Latihan16.java
@@ -0,0 +1,14 @@
+/**
+ * Latihan16
+ */
+public class Latihan16 {
+
+    public static void main(String[] args) {
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama        : R. Rawdhoh Ardhillah");
+        System.out.println("Absen       : 16 ");
+        System.out.println("Kelas       : 1D SIB ");
+        System.out.println("Prodi       : D-IV Sistem Informasi Bisnis");
+        System.out.println("Jurusan     : Teknologi Informasi");
+    }
+}
---
+++ pratikumdaspro01/MyFirstJava16.java
@@ -0,0 +1,12 @@
+public class MyFirstJava16 {
+    public static void main(String[] args) {
+        
+        System.out.println(" ***** IDENTTAS DIRI  *****");
+        System.out.println(" Nama Saya : R. Rawdhoh Ardhillah");
+        System.out.println(" Absen     : 16 ");
+        System.out.println(" Kelas     : 1D");
+        System.out.println("  Prodi    : D4 Sistem Informasi Bisnis");
+        System.out.println(" Jurusan   : Teknologi informasi");
+
+    }
+}
