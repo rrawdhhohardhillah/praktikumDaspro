e11bec3b8fcfb5b5c6183773b1d9fb1226e1ee15
unknown <rrawdhohardhillah@gmail.com>
2024-10-08 10:17:31 +0700

    PemilihanPercoban116

--- Praktikum06/PemilihanPercoban116.java
+++ Praktikum06/PemilihanPercoban116.java
@@ -10,14 +10,20 @@ public class PemilihanPercoban116 {
         int tahun;
         System.out.print("Masukan tahun = ");
         tahun = sc.nextInt();
-
-        if (tahun % 4 == 0 && (tahun % 100 != 0 || tahun % 400 == 0)) {
+        
+        if (tahun % 4 == 0) {
             System.out.println("Tahun Kabisat");
+            if (tahun % 100 == 0) {
+                if (tahun % 400 != 0) {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            }
         } else {
-            System.out.println("Bukan Tahun Kabisat");
+             System.out.println("Bukan Tahun Kabisat");
         }
-            sc.close();
 
-        }
+        sc.close();
     }
+}
+        
 
