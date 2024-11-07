1ccba82897f86563b4898a5b7c535f6b405a87fb
unknown <rrawdhohardhillah@gmail.com>
2024-11-07 22:34:52 +0700

    Triangel16

--- Praktikum08/Triangel16.java
+++ Praktikum08/Triangel16.java
@@ -2,19 +2,21 @@ import java.util.Scanner;
 
 public class Triangel16 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.print( "Masukkan N = ");
+        System.out.print("Masukkan N = ");
         int N = sc.nextInt();
         int i = 0;
         
-        while (i <= N) {
+        while (i < N) {
             int j = 0;
-            while (j < i) {
-                 System.out.print("*");
-                 j++;
+            while (j < i + 1) {
+                System.out.print("*");
+                j++;
             }
+            System.out.println();
             i++;
         }
+        sc.close();
     }
 }
