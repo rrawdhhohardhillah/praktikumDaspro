a455dc0374bf19071bf088296d9b1485b1344ad7
unknown <rrawdhohardhillah@gmail.com>
2024-11-08 09:30:04 +0700

    Praktikum09

--- Praktikum08/Tugas3Nomor1.java
+++ Praktikum08/Tugas3Nomor1.java
@@ -2,20 +2,27 @@ import java.util.Scanner;
 
 public class Tugas3Nomor1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int  ukuran = 5;
+        System.out.print("Masukkan nilai N = ");
+        int N = sc.nextInt();
 
-        for (int i = 0; i < ukuran; i++) {
-            for (int j = 0; j < ukuran; j++) {
-                if ((i == 1 || i == 2 || i == 3) && (j == 1 || j == 2 || j == 3)) {
+        if (N < 3) {
+            System.out.println("Nilai N harus minimal 3");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == 0 || i == N - 1 || j == 0 || j == N - 1) {
+                    System.out.print(N + " ");
+                } else {
                     System.out.print("  ");
-                    continue;
                 }
-                System.out.print("5 ");
             }
             System.out.println();
-        } 
-            sc.close();
+        }
+
+        sc.close();
     }
 }
