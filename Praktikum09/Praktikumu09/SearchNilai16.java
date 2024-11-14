cf59eeb9541d578eecd537e366d6feaf387685f3
unknown <rrawdhohardhillah@gmail.com>
2024-11-14 22:40:46 +0700

    SearchNilai16

--- Praktikum09/Praktikumu09/SearchNilai16.java
+++ Praktikum09/Praktikumu09/SearchNilai16.java
@@ -1,21 +1,36 @@
+import java.util.Scanner;
+
 public class SearchNilai16 {
 
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan banyaknya nilai yang akan di input: ");
+        int jumlahNilai = sc.nextInt();
+
+        int[] arrNilai = new int[jumlahNilai];
+        System.out.println("Masukkan nilai mahasiswa:");
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
         int hasil = -1;
 
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
-                hasil = i;
+                hasil = i + 1;
                 break;
             }
         }
 
         if (hasil != -1) {
-            System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
+            System.out.println("Nilai " + key + " ketemu di mahasiswa ke-" + hasil);
         } else {
-            System.out.println("Nilai " + key + " tidak ditemukan.");
+            System.out.println("Nilai yang dicari tidak ditemukan.");
         }
     }
 }
