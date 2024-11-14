dd0cb446a0ac1a1fe5bab2f341a365d979039ddb
unknown <rrawdhohardhillah@gmail.com>
2024-11-14 19:52:19 +0700

    ArrayNilai16

---
+++ Praktikum09/Praktikumu09/ArrayNilai16.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ArrayNilai16 {
+    public static void main(String[] args) {
+        Scanner sc16 = new Scanner(System.in);
+        int[] nilaiAkhir = new int[10];
+
+        for( int i = 0; i < 10; i++) {
+            System.out.print("Masukkan  nilai akhir ke-"+i+":");
+            nilaiAkhir[i]= sc16.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        }
+    }
+    
+}
