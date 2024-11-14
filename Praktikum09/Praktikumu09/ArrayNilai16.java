79160281850ba75f7e99784feb0d8a719302ef08
unknown <rrawdhohardhillah@gmail.com>
2024-11-14 21:32:56 +0700

    ArrayNilai16

--- Praktikum09/Praktikumu09/ArrayNilai16.java
+++ Praktikum09/Praktikumu09/ArrayNilai16.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 public class ArrayNilai16 {
     public static void main(String[] args) {
         Scanner sc16 = new Scanner(System.in);
+
         int[] nilaiAkhir = new int[10];
 
         for( int i = 0; i < 10; i++) {
-            System.out.print("Masukkan  nilai akhir ke-"+i+":");
-            nilaiAkhir[i]= sc16.nextInt();
+            System.out.print("Masukkan  nilai akhir ke-" + (i + 1) + " : ");
+            nilaiAkhir[i] = sc16.nextInt();
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        for( int i = 0; i < 10; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + (i + 1) + " lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-" + (i + 1) + " tidak lulus.");
+            }
         }
     }
-    
 }
