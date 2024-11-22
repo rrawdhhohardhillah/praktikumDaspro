cbc764df0e09e47a9d90f9cb0ca9bc08d9d4aeff
unknown <rrawdhohardhillah@gmail.com>
2024-11-22 10:31:51 +0700

    Praktikum11

---
+++ Praktikum11/Kafe16.java
@@ -0,0 +1,16 @@
+public class Kafe16{
+    public static void Menu () {
+        System.out.println("==== MENU RESTO KAFE ====");
+        System.out.println("1. kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappuccino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh Tarik - 12,000");
+        System.out.println("5. Roti Bakar - 10,000");
+        System.out.println("6. Mie Goreng - `18,000");
+        System.out.println("=========================");
+        System.out.println("Silahkan pilij menu yang Anda inginkan.");
+    }
+    public static void main(String[] args) {
+        Menu();
+    }
+}
